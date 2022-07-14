825feb09a4d75b5e099c4dc8b1d6eae15d2650d7
rersozlu <ersozlu.rafi@gmail.com>
2022-07-15 02:09:59 +0300

    added the real prices of sdt & angle to check apr

--- contracts/MockOneInch.sol
+++ contracts/MockOneInch.sol
@@ -17,7 +17,6 @@ contract MockOneInch is IAggregationRouterV4 {
     address public constant SDT = 0x73968b9a57c6E53d41345FD57a6E6ae27d6CDB2F;
     address public constant FRAX = 0x853d955aCEf822Db058eb8505911ED77F175b99e;
 
-
     struct Ratios {
         bool registered;
         address dstToken;
@@ -27,8 +26,8 @@ contract MockOneInch is IAggregationRouterV4 {
     mapping(address => Ratios) public mockRatios;
 
     constructor() {
-        mockRatios[ANGL] = Ratios(true, FRAX, 2);
-        mockRatios[SDT] = Ratios(true, FRAX, 4);
+        mockRatios[ANGL] = Ratios(true, FRAX, (32 / 1000) * 1e18);
+        mockRatios[SDT] = Ratios(true, FRAX, (1 / 2) * 1e18);
     }
 
     function swap(
@@ -46,16 +45,30 @@ contract MockOneInch is IAggregationRouterV4 {
             uint256 gasLeft
         )
     {
-        returnAmount = desc.amount * mockRatios[address(desc.srcToken)].ratio;
+        returnAmount =
+            (desc.amount * mockRatios[address(desc.srcToken)].ratio) /
+            1e18;
         spentAmount = desc.amount;
 
-        require(mockRatios[address(desc.srcToken)].registered, "[MOCK][swap] Inavailable in mock.");
-        require(address(desc.dstToken) == mockRatios[address(desc.srcToken)].dstToken, "[MOCK][swap] Inappropriate output.");
-        require(desc.dstToken.balanceOf(address(this)) >= returnAmount, "[MOCK][swap] Not enough output token.");
+        require(
+            mockRatios[address(desc.srcToken)].registered,
+            "[MOCK][swap] Inavailable in mock."
+        );
+        require(
+            address(desc.dstToken) ==
+                mockRatios[address(desc.srcToken)].dstToken,
+            "[MOCK][swap] Inappropriate output."
+        );
+        require(
+            desc.dstToken.balanceOf(address(this)) >= returnAmount,
+            "[MOCK][swap] Not enough output token."
+        );
 
         desc.srcToken.safeTransferFrom(msg.sender, address(this), spentAmount);
 
-        address dstReceiver = desc.dstReceiver == address(0) ? msg.sender : desc.dstReceiver;
+        address dstReceiver = desc.dstReceiver == address(0)
+            ? msg.sender
+            : desc.dstReceiver;
 
         desc.dstToken.safeTransfer(dstReceiver, returnAmount);
         gasLeft = gasleft();
